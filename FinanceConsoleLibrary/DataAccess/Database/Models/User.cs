1b2529c45cab52ea36428e28313bc1440d6c1f1a
Leon Tutte <leonacc@ltutte.de>
2023-05-23 23:52:17 +0200

    Added basic user model

--- FinanceConsoleLibrary/DataAccess/Database/Models/User.cs
+++ FinanceConsoleLibrary/DataAccess/Database/Models/User.cs
@@ -2,5 +2,8 @@ namespace FinanceConsoleLibrary.DataAccess.Database.Models;
 
 public class User
 {
-    
+    public byte Id { get; set; }
+    public string Label { get; set; }
+    public string LoginName { get; set; }
+    public string PasswordHash { get; set; }
 }
