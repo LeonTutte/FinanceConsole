a8bb3dfdd866ac4e2f1b54a60cc2f9728f6d090e
Leon Tutte <leonacc@ltutte.de>
2023-05-23 23:35:49 +0200

    Setup basic database connection

--- FinanceConsoleLibrary/DataAccess/Database/StorageContext.cs
+++ FinanceConsoleLibrary/DataAccess/Database/StorageContext.cs
@@ -1,3 +1,5 @@
+using FinanceConsoleLibrary.Modules.Static;
+using IniParser.Model;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinanceConsoleLibrary.DataAccess.Database;
@@ -6,12 +8,18 @@ public class StorageContext : DbContext
 {
     public StorageContext()
     {
-        
+        LogModule.WriteDebug("Initializing storageContext");
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        base.OnConfiguring(optionsBuilder);
+        // TODO: Add password to connection string
+        LogModule.WriteDebug("Configuring storageContext");
+        IniData settingsData = ConfigurationModule.GetConfiguration();
+        LogModule.WriteDebug("Building connection string for storageContext");
+        var connectionString = $"Data Source={settingsData["Database"]["Filename"]};";
+        optionsBuilder.UseSqlite(connectionString);
+        LogModule.WriteDebug("Finished configuring storageContext");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
